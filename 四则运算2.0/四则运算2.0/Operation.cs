a5a843279f68de6deb51b1f4220c88f0e0b3bfd1
Administrator <Administrator@ts70720>
2019-03-19 16:08:57 +0800

    四则运算

---
+++ 四则运算2.0/四则运算2.0/Operation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 四则运算2._0
+{
+     public class Operation
+    {
+        /// <summary>
+        /// 一个运算符
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="number"></param>
+        /// <param name="difficulty"></param>
+        /// <param name="operatos"></param>
+        /// <param name="operatos1"></param>
+        /// <returns></returns>
+        public static string formula1(DataTable dt, Random number, int difficulty, string[] operatos, string[] operatos1)
+        {
+            string number1 = number.Next(0, difficulty).ToString();
+            string number2 = number.Next(0, difficulty).ToString();
+            int number_op = number.Next(0, 4);
+            string results = dt.Compute(number1 + operatos1[number_op] + number2, "false").ToString();
+            string results1 = number1 + operatos[number_op] + number2 + "=" + results;
+            if (Convert.ToDouble(results) < 0|| results == "∞")
+            {
+                results1 = formula1(dt, number, difficulty, operatos, operatos1);
+            }
+            return results1;
+        }
+
+        /// <summary>
+        /// 两个运算符
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="number"></param>
+        /// <param name="difficulty"></param>
+        /// <param name="operatos"></param>
+        /// <param name="operatos1"></param>
+        /// <returns></returns>
+        public static string formula2(DataTable dt, Random number, int difficulty, string[] operatos, string[] operatos1)
+        {
+            string number1 = number.Next(0, difficulty).ToString();
+            string number2 = number.Next(0, difficulty).ToString();
+            string number3 = number.Next(0, difficulty).ToString();
+            int number_op = number.Next(0, 4);
+            int number_op1 = number.Next(0, 4);
+            string results = dt.Compute(number1 + operatos1[number_op] + number2 + operatos1[number_op1] + number3, "false").ToString();
+            string results1 = number1 + operatos[number_op] + number2 + operatos1[number_op1] + number3 + "=" + results;
+            if (Convert.ToDouble(results) < 0 || results == "∞")
+            {
+                results1 = formula2(dt, number, difficulty, operatos, operatos1);
+            }
+            return results1;
+        }
+        /// <summary>
+        /// 三个运算符
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="number"></param>
+        /// <param name="difficulty"></param>
+        /// <param name="operatos"></param>
+        /// <param name="operatos1"></param>
+        /// <returns></returns>
+        public static string formula3(DataTable dt, Random number, int difficulty, string[] operatos, string[] operatos1)
+        {
+            string number1 = number.Next(0, difficulty).ToString();
+            string number2 = number.Next(0, difficulty).ToString();
+            string number3 = number.Next(0, difficulty).ToString();
+            string number4 = number.Next(0, difficulty).ToString();
+            int number_op = number.Next(0, 4);
+            int number_op1 = number.Next(0, 4);
+            int number_op2 = number.Next(0, 4);
+            string results = dt.Compute(number1 + operatos1[number_op] + number2 + operatos1[number_op1] + number3 + operatos1[number_op2] + number4, "false").ToString();
+            string results1 = number1 + operatos[number_op] + number2 + operatos[number_op1] + number3 + operatos[number_op2] + number4 + "=" + results;
+            if (Convert.ToDouble(results) < 0 || results == "∞")
+            {
+                results1 = formula3(dt, number, difficulty, operatos, operatos1);
+            }
+            return results1;
+        }
+    }
+}
---
+++ 四则运算2.0/四则运算2.0/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 四则运算2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            Random number = new Random(); //实例化一个随机数           
+            string[] operatos = new string[] { "＋", "－", "×", "÷" };
+            string[] operatos1 = new string[] { "+", "-", "*", "/" };
+            Console.WriteLine("要生成多少题，最多支持10000题");
+            int draw = Convert.ToInt32(Console.ReadLine()); //获取出题量
+            Console.WriteLine("要生多少以内的计算题");
+            int difficulty = Convert.ToInt32(Console.ReadLine()); //获取出题量
+            for (int s = 0; s < draw; s++)
+            {
+                int nuber_ti = number.Next(0, 3);
+                switch (nuber_ti)
+                {
+                    case 0:
+                        Console.WriteLine(Operation.formula1(dt, number, difficulty, operatos, operatos1));
+                        break;
+                    case 1:
+                        Console.WriteLine(Operation.formula2(dt, number, difficulty, operatos, operatos1));
+                        break;
+                    case 2:
+                        Console.WriteLine(Operation.formula3(dt, number, difficulty, operatos, operatos1));
+                        break;
+                }
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
